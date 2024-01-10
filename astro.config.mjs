3821e2fcba56abcd39c8faacf40080f035df0032
ANTONIIOSC7\anton <antonio.saborido01@gmail.com>
2024-01-10 21:44:26 +0100

    Añadido github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const SERVER_PORT = 3000;
+const LIVE_URL = "https://antoniiosc7.github.io";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  server: { port: SERVER_PORT },
+  site: LIVE_URL
 });
