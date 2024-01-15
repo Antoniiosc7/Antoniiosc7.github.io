5a791ea4d849e10e1f05f76cafd37a57c269aa8b
ANTONIIOSC7\anton <antonio.saborido01@gmail.com>
2024-01-15 20:44:50 +0100

    Implementado cambio de idioma y modo oscuro

---
+++ src/constsES.ts
@@ -0,0 +1,36 @@
+export const SOCIAL_LINKS = [
+  {name: 'linkedin', url:'https://www.linkedin.com/in/antonio-saborido-campos-512343204/'},
+  {name: 'email', url:'mailto:antonio.saborido01@gmail.com'},
+  {name:'github', url:'https://github.com/antoniiosc7'}
+]
+
+export const FEATURED_REPOSITORIES = [
+    {
+      id: 0,
+      name: 'Jump Marius',
+      url: 'https://github.com/Antoniiosc7/JumpMarius',
+      description: '2D platformer game created using the Python library, Pygame.',
+      language: 'python',
+    },
+    {
+      id: 1,
+      name: 'Twitch Clips',
+      url: 'https://github.com/Antoniiosc7/TwitchClips',
+      description: 'Integration of the Twitch API into a Svelte page that allows viewing the most popular clips from League of Legends streamers.',
+      language: 'svelte',
+    },
+    {
+      id: 2,
+      name: 'Jersey Detection',
+      url: 'https://github.com/Antoniiosc7/JerseyDetection',
+      description: 'Text detection in images, primarily based on football jerseys to identify the player.',
+      language: 'python',
+    },
+    {
+      id: 3,
+      name: 'Api Tennis',
+      url: 'https://github.com/Antoniiosc7/TFG-Server',
+      description: 'This repository creates an API using Javascript and Expressjs for tennis statistics.',
+      language: 'javascript',
+    },
+]
